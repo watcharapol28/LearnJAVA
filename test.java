257c7a74d0d81a5ef5a10a79123e4bc472bcb1b0
Watcahrapol <watcharapol.tree@kmutt.ac.th>
2022-02-21 20:19:46 +0700

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,12 @@
-class Suvscribe{
+import java.util.Scanner;
+
+public class test{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        int value;
+        System.out.println("\tHello World!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Value : ");
+        value = in.nextInt();
+        System.out.println("This Value is : " + value);
     }
 }
